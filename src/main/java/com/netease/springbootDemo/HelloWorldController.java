25208ccd07c9cce159132d2e3338d5095ee0827e
bjchenyuan <bjchenyuan@corp.netease.com>
2017-01-13 20:37:43 +0800

    整合数据库

--- src/main/java/com/netease/springbootDemo/HelloWorldController.java
+++ src/main/java/com/netease/springbootDemo/HelloWorldController.java
@@ -1,28 +1,42 @@
 package com.netease.springbootDemo;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.netease.springbootDemo.service.CityService;
+import com.netease.springbootDemo.vo.CityVo;
 
 /**
  * Hello world!
  *
  */
 @RestController
-public class HelloWorldController
-{
+public class HelloWorldController {
 	private static final Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
 
+	@Autowired
+	private CityService cityService;
+
 	@RequestMapping("/index/{name}")
-	public String index(@PathVariable String name)
-	{
-		if (null == name)
-		{
+	public String index(@PathVariable String name) {
+		if (null == name) {
 			name = "boy";
 		}
 		logger.info("name={}", name);
 		return "hello world" + name;
 	}
+
+	@RequestMapping("/city/findAll.html")
+	public String findCity() {
+		List<CityVo> cityList = cityService.findAll();
+		logger.info("cityList:{}", cityList);
+		return cityList.toString();
+	}
+
 }
--- src/main/java/com/netease/springbootDemo/MainDemo.java
+++ src/main/java/com/netease/springbootDemo/MainDemo.java
@@ -2,10 +2,13 @@ package com.netease.springbootDemo;
 
 import org.springframework.boot.SpringApplication;
 
+import com.netease.springbootDemo.config.ConfigBean;
+
 public class MainDemo {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigBean.class, args);
+		SpringApplication app = new SpringApplication();
+		app.run(ConfigBean.class, args);
 	}
 
 }
